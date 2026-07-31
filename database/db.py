8abda29079e08584f79f4aaf4f44e7eb4b024680
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-07-31 17:06:14 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -17,3 +17,28 @@ def get_connection():
     conn.row_factory = sqlite3.Row
 
     return conn
+    def get_interaction(drug1_id, drug2_id):
+
+    conn = get_connection()
+
+    row = conn.execute(
+        """
+        SELECT *
+        FROM interactions
+        WHERE
+        ("Drug1 ID"=? AND "Drug2 ID"=?)
+        OR
+        ("Drug1 ID"=? AND "Drug2 ID"=?)
+        LIMIT 1
+        """,
+        (
+            drug1_id,
+            drug2_id,
+            drug2_id,
+            drug1_id
+        )
+    ).fetchone()
+
+    conn.close()
+
+    return row
