6e9d454a6bc75ea107d105202b9dfca55f4ae604
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-05 11:15:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,15 +21,6 @@ from utils.clinical_engine import build_report, render_report
 # تهيئة محرك معلومات الأدوية
 drug_info_engine = DrugInfoEngine()
 
-# 🔍 تشخيص محرك معلومات الأدوية
-st.write("Number of drugs:", len(drug_info_engine.drugs))
-
-first_key = next(iter(drug_info_engine.drugs))
-st.write("First key:", first_key)
-
-st.write("First record:")
-st.json(drug_info_engine.drugs[first_key])
-
 
 class DrugFoodInteractionEngine:
 
